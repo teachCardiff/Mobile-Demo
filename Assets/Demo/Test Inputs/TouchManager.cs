464ac7674ee0a5f2dad1fb310db100f1dd44b591
teachCardiff <mason.cardiff@comalisd.org>
2026-01-12 10:04:03 -0600

    Clean up project

--- Assets/Demo/Test Inputs/TouchManager.cs
+++ Assets/Demo/Test Inputs/TouchManager.cs
@@ -6,17 +6,23 @@ namespace Mobile_Demo
     public class TouchManager : MonoBehaviour
     {
         [SerializeField] GameObject player;
+        [SerializeField] bool useVirtualJoystick;
+        [SerializeField] float moveSpeed;
+        Vector2 moveInput;
         private PlayerInput playerInput;
         Vector2 newPos;
         bool isDragging;
+
 
         private void Awake()
         {
             playerInput = GetComponent<PlayerInput>();
         }
-
+#region SendMessages
         void OnTouchPosition(InputValue value)
         {
+            if (useVirtualJoystick) return;
+            
             // Test to verify a position input
             newPos = value.Get<Vector2>();
             Debug.Log(newPos);
@@ -26,6 +32,41 @@ namespace Mobile_Demo
             position.z = player.transform.position.z;
             player.transform.position = position;
         }
+
+        void OnTouchPress(InputValue value)
+        {
+            if (useVirtualJoystick) return;
+
+            // Test to verify a touch input
+            float val = value.Get<float>();
+            Debug.Log(val);
+
+            // Move the object to the position of the touch
+            Vector3 position = Camera.main.ScreenToWorldPoint(newPos);
+            position.z = player.transform.position.z;
+            player.transform.position = position;
+
+        }
+
+        void OnMove(InputValue value)
+        {
+            if (useVirtualJoystick)
+            {
+                moveInput = value.Get<Vector2>();
+            }
+        }
+
+        void Update()
+        {
+            if (useVirtualJoystick)
+            {
+                Vector3 delta = new Vector3(moveInput.x, moveInput.y, 0f) * moveSpeed * Time.deltaTime;
+                player.transform.position += delta;
+            }
+        }
+        #endregion
+
+        #region UnityEvents
 
         // Change the input type to allow us to determine the state of the input
         // i.e. canceled, performed, or started
@@ -64,19 +105,6 @@ namespace Mobile_Demo
                 Debug.Log("Position action canceled. Trigger effects!");
             }
         }
-
-        void OnTouchPress(InputValue value)
-        {
-            // Test to verify a touch input
-            // float val = value.Get<float>();
-            // Debug.Log(val);
-
-            // Move the object to the position of the touch
-            // Vector3 position = Camera.main.ScreenToWorldPoint(newPos);
-            // position.z = player.transform.position.z;
-            // player.transform.position = position;
-
-        }
+#endregion
     }
-
 }
